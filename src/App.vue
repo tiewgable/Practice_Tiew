338e316411302f49231b863df8e92c5a7bbf030a
Tiewg <132216660+tiewgable@users.noreply.github.com>
2023-05-05 14:04:25 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/vmodel">V-Model</router-link> |
+    <router-link to="/von">V-Model</router-link> |
+    <router-link to="/vbind">V-Bind</router-link> |
+    <router-link to="/prop">prop</router-link> |
+    <router-link to="/axios">axios</router-link> | <br /><br />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router-link-exact-active {
+  color: red;
+}
+</style>
---
+++ src/components/axios.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-for="manu in manu" :key="manu.id">{{ manu.id }}-{{ manu.Title }}</p>
+    <input placeholder="id" v-model="id" />
+    <input placeholder="Title" v-model="Title" />
+    <button @click="post()">post</button>
+    <button @click="put()">Put</button>
+    <button @click="remove()">Delete</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      text: "test",
+      manufacturer: [{ id: 1, Title: "test" }],
+    };
+  },
+  created() {
+    this.get();
+  },
+  methods: {
+    get() {
+      axios.get("").then((respone).then((respone => {this.manufacturer = respone.data)
+      
+    },
+  },
+};
+</script>
